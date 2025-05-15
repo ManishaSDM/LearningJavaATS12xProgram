55ca0f1617e48b3d5fa0b3a16ffc77fa9c4824fc
ManishaSDM <manisha.shindeistqbf@gmail.com>
2025-05-15 06:41:01 -0500

    New labs added after 11th May2025

---
+++ src/Tasks/Task1_Table_of_5.java
@@ -0,0 +1,30 @@
+package Tasks;
+
+public class Task1_Table_of_5 {
+    public static void main(String[] args) {
+
+        int num = 5;
+        System.out.printf ("%d", num );
+        System.out.println();
+        //System.out.printf ( String.valueOf(num *2) );
+        System.out.printf ("%d", num*2 );
+        System.out.println();
+        System.out.printf ("%d", num*3 );
+        System.out.println();
+        System.out.printf ("%d", num*4 );
+        System.out.println();
+        System.out.printf ("%d", num*5 );
+        System.out.println();
+        System.out.printf ("%d", num*6 );
+        System.out.println();
+        System.out.printf ("%d", num*7 );
+        System.out.println();
+        System.out.printf ("%d", num*8 );
+        System.out.println();
+        System.out.printf ("%d", num*9);
+        System.out.println();
+        System.out.printf ("%d", num*10 );
+        System.out.println();
+
+    }
+}
