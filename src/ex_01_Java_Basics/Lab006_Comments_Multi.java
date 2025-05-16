e21bd34f29254bcbd6889f56c931d6973f56a144
ManishaSDM <manisha.shindeistqbf@gmail.com>
2025-05-16 10:11:35 -0500

    New labs added after 11th May2025

---
+++ src/ex_01_Java_Basics/Lab006_Comments_Multi.java
@@ -0,0 +1,30 @@
+package ex_01_Java_Basics;
+/**
+ * Author : Manisha S
+ * Course :  ATB12x
+ * Learning :  Java
+ *
+ **/
+public class Lab006_Comments_Multi {
+/**
+ * Author : Manisha S
+ * Course :  ATB12x
+ * Learning :  Java
+ *
+ **/
+public static void main(String[] args) {
+    /**
+     * Author : Manisha S
+     * Course :  ATB12x
+     * Learning :  Java
+     *
+     **/
+    System.out.println("Multi Comment");
+}
+}
+/**
+ * Author : Manisha S
+ * Course :  ATB12x
+ * Learning :  Java
+ *
+ **/
