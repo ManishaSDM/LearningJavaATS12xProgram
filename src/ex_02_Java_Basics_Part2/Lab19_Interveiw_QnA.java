064fb72d45544ea8021f212254ba8c59d4b3f290
ManishaSDM <manisha.shindeistqbf@gmail.com>
2025-05-19 22:37:12 -0500

    Add files via upload

---
+++ src/ex_02_Java_Basics_Part2/Lab19_Interveiw_QnA.java
@@ -0,0 +1,11 @@
+package ex_02_Java_Basics_Part2;
+
+public class Lab19_Interveiw_QnA {
+    public static void main(String[] args) {
+        int _2 = 123;
+        System.out.println(_2);
+        int _1 = 1234;
+        System.out.println(_1);
+        int _0 = 123;
+    }
+}
---
+++ src/ex_02_Java_Basics_Part2/Lab20_Interveiw_QnA.java
@@ -0,0 +1,14 @@
+package ex_02_Java_Basics_Part2;
+
+public class Lab20_Interveiw_QnA {
+    public static void main(String[] args) {
+        // int enum = 10;
+        int Enum = 10;
+        int a_b = 89;
+//        int pramod dutta  = 89;
+        int pramod$dutta = 90;
+        int Lab020_Interview_QnA2 = 123;
+
+
+    }
+}
