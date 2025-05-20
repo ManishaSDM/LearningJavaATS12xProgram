e2de1c2a016f5f67587355ea3b289805aef0c231
ManishaSDM <manisha.shindeistqbf@gmail.com>
2025-05-19 22:35:43 -0500

    Add files via upload

---
+++ src/ex_01_Java_Basics/Lab13_Println_vs_Print.java
@@ -0,0 +1,13 @@
+package ex_01_Java_Basics;
+
+public class Lab13_Println_vs_Print {
+    public static void main(String[] args) {
+        //        System.out.println("Hi"); // newline
+ //System.out.println("Bye");
+
+        System.out.print("Hi"); // newline
+        System.out.println("Bye");
+
+
+    }
+}
