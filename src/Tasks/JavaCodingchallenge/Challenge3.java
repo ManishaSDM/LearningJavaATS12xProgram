552623cbb47d364f882e55b2b9afbf4d021126fe
ManishaSDM <manisha.shindeistqbf@gmail.com>
2025-06-21 01:43:24 -0500

    Javachallenge3

---
+++ src/Tasks/JavaCodingchallenge/Challenge3.java
@@ -0,0 +1,20 @@
+package Tasks.JavaCodingchallenge;
+
+public class Challenge3 {
+    public static void main(String[] args) {
+        int a=10;
+        int b= 5;
+//- Use comparison operators (==, !=, <, >, <=, >=)
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a<b);
+        System.out.println(a>b);
+        System.out.println(a<=b);
+        System.out.println(a>=b);
+      //  - Use logical operators (&&, ||, !)
+        System.out.println((a > b) && (a > 0));
+        System.out.println((a < b) || (a > 0));
+        System.out.println(!(a > b));
+
+    }
+}
