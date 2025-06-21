b56d6fc73a821327fcf3243b5a92ce85d578540b
ManishaSDM <manisha.shindeistqbf@gmail.com>
2025-06-21 01:29:19 -0500

    Javachallenge2

---
+++ src/Tasks/JavaCodingchallenge/Challenge2.java
@@ -0,0 +1,33 @@
+package Tasks.JavaCodingchallenge;
+
+public class Challenge2 {
+    public static void main(String[] args) {
+        // Arithmetic Operators
+        // +,-,%,/,*
+        int a = 10;
+        int b = 20;
+        System.out.println(a + b);//addition
+        System.out.println(a > b);//a grester than b
+        System.out.println(a >= b);
+        System.out.println(a == b);
+        System.out.println(a != b);
+        System.out.println(a%b);//a modudulus b
+
+
+
+
+        // Compound Assignment Operators
+        int age = 10;
+        age += 10;   // age = age+10
+        System.out.println(age);
+        age /=10; // age = age/10
+        System.out.println(age);
+        age-=10; //age=age-10=-8
+        System.out.println(age);
+        age*=10;//age=age*10
+        System.out.println(age);
+
+
+
+    }
+}
